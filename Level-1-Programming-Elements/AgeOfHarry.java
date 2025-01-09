a0bc398d0b73effb53d01bb9e0c2d2ce59ed95da
Rishav <rishavhimself@gmail.com>
2025-01-09 16:56:17 +0530

    Question 1

---
+++ Level-1-Programming-Elements/AgeOfHarry.java
@@ -0,0 +1,11 @@
+// Creating a class named AgeOfHarry indicating the purpose of creating the class
+class AgeOfHarry{
+public static void main(String[] args){
+// Creating a intgeter variable currentYear indicating the present year 
+int currentYear = 2024; 
+//Creating a intgeter variable named birthyear indicating Harry's year of birth 
+int birthYear = 2000; 
+//Displaying the current age of Harry 
+System.out.println("Harry's age in 2024 is: " + (currentYear-birthYear)); 
+}
+}
