ffcd9741ed2e795d2d212a91623e47233684b07b
Rishav <rishavhimself@gmail.com>
2025-01-09 16:57:22 +0530

    Question 3

---
+++ Level-1-Programming-Elements/KmtoMiles.java
@@ -0,0 +1,9 @@
+// creating a class for converting distance from km to miles
+class KmtoMiles{
+public static void main (String[] args){
+// creating a double type variable to store floating point value of distance 
+double distanceInMiles = (double) 10.8 * (double) 1.6 ; 
+// Displaying the required result
+System.out.println("The distance " + 10.8 +"km in miles is " + distanceInMiles); 
+}
+}
