85e657d214b4540ca18a074bb04531e848cd53c5
Rishav <rishavhimself@gmail.com>
2025-01-11 16:11:19 +0530

    Question 3

---
+++ Level-1-Methods/Handshakes.java
@@ -0,0 +1,23 @@
+// Import Scanner class from util package for user input
+import java.util.Scanner;
+// Create HandshakesByMethods Class to find number of hand shakes
+public class HandshakesByMethods {
+
+    public static void main(String[] args) {
+		
+		// Create a Scanner object for taking input
+        Scanner scanner = new Scanner(System.in);
+		
+		// Ask the user to enter the number of students
+        System.out.print("Enter number of students: ");
+        int students = scanner.nextInt();
+		
+		int handshakes =(numberOfStudents * (numberOfStudents - 1)) / 2;
+		
+		// Displaying result
+        System.out.println("The maximum number of handshakes is " + handshakes);
+        
+		// Close the scanner
+		scanner.close();
+    }
+}
