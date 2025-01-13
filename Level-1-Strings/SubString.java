d00b080164ae17553c7387f602872ffa6111ae7f
Rishav Raj <144218227+rishavhimself@users.noreply.github.com>
2025-01-13 23:57:03 +0530

    Added Comments in SubString.java

--- Level-1-Strings/SubString.java
+++ Level-1-Strings/SubString.java
@@ -1,6 +1,6 @@
 // Import Scanner class from util package for user input
 import java.util.Scanner;
-// Create SubString Class to compute length of line
+// Create SubString Class to create sub String
 public class SubString {
 	public static String generateSubString(String string, int start, int end){
 		String temp="";
@@ -26,19 +26,30 @@ public class SubString {
 	
 	public static void main(String args[]){	
 		
+		 // Create a Scanner object for taking input
 		Scanner scanner = new Scanner(System.in);
 		
+		// Ask the user to enter the string
 		System.out.println("Enter String");
 		String string = scanner.nextLine();
+		
+		// Ask the user to enter the start index
 		System.out.println("Enter Start Index");
 		int start = scanner.nextInt();
+		
+		// Ask the user to enter the end index
 		System.out.println("Enter End Index");
 		int end= scanner.nextInt();
 		
+		//Method calling
 		String firstSubString = generateSubString(string, start, end);
 		String secondSubString = string.substring(start,end);
 		boolean result=isEqual(firstSubString,secondSubString);
 		
+		//Display result
 		System.out.println("The result of checking whether the two sub strings are equal by a method is : "+ result);
+	
+		// Close the scanner
+		scanner.close();
 	}
 }
