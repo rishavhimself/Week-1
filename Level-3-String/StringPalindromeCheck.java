3d45ab624fce6129cd105d091f3f35e0cc54ea5d
Rishav <rishavhimself@gmail.com>
2025-01-15 19:23:38 +0530

    Question 7

---
+++ Level-3-String/StringPalindromeCheck.java
@@ -0,0 +1,33 @@
+// Import Scanner class from util package for user input
+import java.util.Scanner;
+// Create StringPalindromeCheck Class to check String Palindrome
+public class StringPalindromeCheck {
+	
+	public static void main(String args[]){
+		
+		 // Create a Scanner object for taking input
+		Scanner scanner =new Scanner(System.in);
+		
+		// Ask the user to enter the string
+		System.out.println("Enter a String");
+		String string = scanner.nextLine();
+		boolean flag = true;
+		
+		int start=0,end=string.length()-1;
+		while(start<=end){
+			if(string.charAt(start)!=string.charAt(end)){
+				flag=false;
+				break;
+			}
+			
+			start++;end--;
+		}
+		
+		//Display Result
+		if(flag)System.out.println("The given string is palindrome");
+		else System.out.println("The given string is not palindrome");
+ 
+		// Close the scanner
+		scanner.close()
+	}
+}
