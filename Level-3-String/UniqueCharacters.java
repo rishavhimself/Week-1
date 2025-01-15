2d8464fdec33d9e3daf1ef7858ef4c1cb493cede
Rishav <rishavhimself@gmail.com>
2025-01-15 19:22:19 +0530

    Question 2

---
+++ Level-3-String/UniqueCharacters.java
@@ -0,0 +1,29 @@
+// Import Scanner class from util package for user input
+import java.util.*;
+// Create UniqueCharacters Class to remove duplicates 
+public class UniqueCharacters {
+	
+	public static void main(String args[]){
+		
+		 // Create a Scanner object for taking input
+		Scanner scanner =new Scanner(System.in);
+		
+		// Ask the user to enter the string
+		System.out.println("Enter a String");
+		String string = scanner.nextLine();
+		string.toLowerCase();
+		
+		//implementing HashSet as it doesn't allow duplicates
+		HashSet <Character> hashset = new HashSet<>();
+		for(int i=0;i<string.length();i++){
+			hashset.add(string.charAt(i));
+		}
+		
+		//Display Result
+		for(char character : hashset)
+		System.out.print(character+" ");
+		
+		// Close the scanner
+		scanner.close();
+	}
+}
